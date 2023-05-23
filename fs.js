68a95d3213e15c56db2dbe183ec90ba9b0e32677
rottencarrot33 <122114827+rottencarrot33@users.noreply.github.com>
2023-05-23 13:50:20 +0300

    Update fs.js

--- fs.js
+++ fs.js
@@ -1,4 +1,13 @@
-
 setTimeout(function () {
     window.location.href = 'https://rottencarrot33.github.io/kassandra-trips/';
 }, 3 * 60 * 1000);
+
+
+// Find the <ul> element
+var ulElement = document.querySelector('ul');
+
+// Find the first <li> element within the <ul>
+var liElement = ulElement.querySelector('li');
+
+// Add '(bus stop)' to the first <li> element
+liElement.textContent += ' (Bus stop)';
